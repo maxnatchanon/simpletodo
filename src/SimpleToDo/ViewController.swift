afd6730906b694651a042bacf4db1f5882289a41
Natchanon A <natchanon.347@gmail.com>
2018-10-21 21:14:08 +0700

    Create Xcode project

---
+++ src/SimpleToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleToDo
+//
+//  Created by Natchanon A. on 21/10/2561 BE.
+//  Copyright © 2561 Natchanon A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
